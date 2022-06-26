39fd6b98d1c4e6f661f7b35d2b45af0266dc3389
dreessen-n <dreessen.edu@gmail.com>
2022-06-25 20:38:15 -0600

    Update with ability to run both versions

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def render_table():
+def render_table_bootstrap_ver():
     users = [
         {'first_name': 'Michael', 'last_name': 'Choi'},
         {'first_name': 'John', 'last_name': 'Supsupin'},
@@ -11,6 +11,17 @@ def render_table():
         {'first_name': 'KB', 'last_name': 'Tonel'}
     ]
     return render_template('index_bootstrap.html', users=users)
+
+
+@app.route('/table')
+def render_table():
+    users = [
+        {'first_name': 'Michael', 'last_name': 'Choi'},
+        {'first_name': 'John', 'last_name': 'Supsupin'},
+        {'first_name': 'Mark', 'last_name': 'Guillen'},
+        {'first_name': 'KB', 'last_name': 'Tonel'}
+    ]
+    return render_template('index_table.html', users=users)
 
 # Error message for 404
 @app.errorhandler(404)
