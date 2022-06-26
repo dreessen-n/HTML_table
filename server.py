63eebc95db7a2aac16f0354810e053620fcdf899
dreessen-n <dreessen.edu@gmail.com>
2022-06-25 18:50:40 -0600

    Update with solution;

--- server.py
+++ server.py
@@ -3,8 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def render_table():
+    users = [
+        {'first_name': 'Michael', 'last_name': 'Choi'},
+        {'first_name': 'John', 'last_name': 'Supsupin'},
+        {'first_name': 'Mark', 'last_name': 'Guillen'},
+        {'first_name': 'KB', 'last_name': 'Tonel'}
+    ]
+    return render_template('index.html', users=users)
 
 # Error message for 404
 @app.errorhandler(404)
